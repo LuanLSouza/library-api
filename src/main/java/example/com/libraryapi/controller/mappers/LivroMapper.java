b19fad0145d5486d80fd62401dab03bad0e760b4
LuanLSouza <luanlimas328@gmail.com>
2025-04-09 18:50:21 -0300

    Implemented mapstruct

---
+++ src/main/java/example/com/libraryapi/controller/mappers/LivroMapper.java
@@ -0,0 +1,18 @@
+package example.com.libraryapi.controller.mappers;
+
+import example.com.libraryapi.controller.dto.CadastroLivroDTO;
+import example.com.libraryapi.model.Livro;
+import example.com.libraryapi.repository.AutorRepository;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Mapper(componentModel = "spring")
+public abstract class LivroMapper {
+
+    @Autowired
+    AutorRepository autorRepository;
+
+    @Mapping(target = "autor", expression = "java( autorRepository.findById(dto.idAutor()).orElse(null) )")
+    public abstract Livro toEntity(CadastroLivroDTO dto);
+}
